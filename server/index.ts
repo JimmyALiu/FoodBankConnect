2d62876786c72271176dbd3bc82128d312276750
Jimmy Liu <jimmyliu20040@gmail.com>
2025-05-06 00:51:29 -0700

    Modify default server slightly with better comments.

--- server/index.ts
+++ server/index.ts
@@ -4,15 +4,15 @@ import express from 'express';
 // Initialize the express engine
 const app: express.Application = express();
  
-// Take a port 3000 for running server.
 const port: number = 3000;
  
 // Handling '/' Request
 app.get('/', (_req, _res) => {
     _res.send("TypeScript With Express");
-});
+}); 
  
 // Server setup
+// will run once the server starts
 app.listen(port, () => {
     console.log(`TypeScript with Express 
          http://localhost:${port}/`);
