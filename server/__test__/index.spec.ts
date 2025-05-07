907a89082bd5956529c49f075a1939d9785ae8e4
Kyle <kb0531@cs.washington.edu>
2025-05-07 00:28:55 -0700

    added Kyle's test cases for server

--- server/__test__/index.spec.ts
+++ server/__test__/index.spec.ts
@@ -1,3 +1,9 @@
+import { multiply } from '../index'
+
 test('dummy test', () => {
     expect(true).toBe(true);
+});
+    
+test('multiply two numbers correctly', () => {
+    expect(multiply(3, 5)).toBe(15);
 });
