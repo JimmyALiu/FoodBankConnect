0e4f22e6d59ded28b7acd55914a5e294b39b83f0
blobgoat <jacobaaronseaman@gmail.com>
2025-05-08 00:50:50 -0700

    first attempt of making google api work

--- client/src/intake_forms/client_intake.ts
+++ client/src/intake_forms/client_intake.ts
@@ -1,7 +1,6 @@
 // sheets.js
-import { google, } from 'googleapis';
+import { google, sheets_v4 } from 'googleapis';
 import { JWT } from 'google-auth-library';
-import * as sheetsTypes from 'googleapis/build/src/apis/sheets/v4';
 import type { GaxiosResponse } from 'gaxios';
 
 // Load credentials from service account JSON
@@ -12,13 +11,13 @@ const auth = new JWT({
 
 async function readSheet() {
     // Create a JWT client
-    const sheets: sheetsTypes.sheets_v4.Sheets = google.sheets({ version: 'v4', auth });
+    const sheets: sheets_v4.Sheets = google.sheets({ version: 'v4', auth });
 
     const spreadsheetId: string = '1DXaJkja09LL5AMpoNKFsxeJfx8GcNZ1v9qzPYzDlvW0';
     const range: string = 'Sheet1!A1:D10'; // ← replace with the range you want to read
 
     try {
-        const res: GaxiosResponse<sheetsTypes.sheets_v4.Schema$ValueRange> = await sheets.spreadsheets.values.get({
+        const res: GaxiosResponse<sheets_v4.Schema$ValueRange> = await sheets.spreadsheets.values.get({
             spreadsheetId,
             range,
         });
