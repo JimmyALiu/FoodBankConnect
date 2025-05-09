b03b9f8ccb327741417ac5557088bba73fc4ab2b
Kyle <kb0531@cs.washington.edu>
2025-05-09 01:39:08 -0700

    Remove redundant jest.config.cjs (config now in package.json)

--- client/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__test__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-};
