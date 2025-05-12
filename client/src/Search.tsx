2f7e5735d7ba57fd098620dae5a14748961a2b83
Jimmy Liu <jimmyliu20040@gmail.com>
2025-05-12 16:11:52 -0700

    Fixing unnecessary nav button styles and added darker color on hover.

--- client/src/Search.tsx
+++ client/src/Search.tsx
@@ -14,13 +14,13 @@ function Search({
         </p>
         <div className="flex space-x-4">
           <button
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+            className="px-4 py-2 text-blue-500 hover:text-blue-700 transition"
             onClick={() => setCurrentPage("Home")}
           >
             Go to Home
           </button>
           <button
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+            className="px-4 py-2 text-blue-500 hover:text-blue-700 transition"
             onClick={() => setCurrentPage("Flagged")}
           >
             View Flagged Entries
