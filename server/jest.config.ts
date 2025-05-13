b7a25af30fd3d7c4101577645b5f9044e9d7f7b6
Kyle <kb0531@cs.washington.edu>
2025-05-12 20:31:02 -0700

    updated jest.config.ts

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,22 +1,34 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
-const config: JestConfigWithTsJest = {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts$': [
-            'ts-jest',
-            {
-                useESM: true,
-                tsconfig: './tsconfig.json'
-            },
-        ],
-    },
-    extensionsToTreatAsEsm: ['.ts'],
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.ts$': '$1',
-    },
-    testPathIgnorePatterns: ['/build/', '/dist/']
+const common: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.ts$': '$1',
+  },
 };
 
-export default config;
+export default {
+  projects: [
+    {
+      ...common,
+      displayName: 'unit',
+      testMatch: ['<rootDir>/__test__/**/*.test.ts', '<rootDir>/__test__/**/*.spec.ts'],
+    },
+    {
+      ...common,
+      displayName: 'integration',
+      testMatch: ['<rootDir>/__test__/**/*.int.test.ts'],
+    },
+  ],
+};
