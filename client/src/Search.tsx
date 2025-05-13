10b15fed6272e8be32e48c8897d3e91ead125179
Jimmy Liu <jimmyliu20040@gmail.com>
2025-05-13 00:48:00 -0700

    Style search form.

--- client/src/Search.tsx
+++ client/src/Search.tsx
@@ -46,16 +46,16 @@ function Search({
             View Flagged Entries
           </button>
         </div>
-        <form action={searchDatabase}>
+        <form action={searchDatabase} className="pt-[2%] flex flex-col gap-2">
           <label htmlFor="fname">First name:</label>
-          <input id="fname" name="firstName" type="text" value={formData.firstName} onChange={handleChange} className="border" />
+          <input id="fname" name="firstName" type="text" value={formData.firstName} onChange={handleChange} className="border-2 border-blue-300 rounded-md focus:border-blue-400 focus:outline-none transition duration-250" />
           <label htmlFor="lname">Last name:</label>
-          <input id="lname" name="lastName" type="text" value={formData.lastName} onChange={handleChange} className="border" />
+          <input id="lname" name="lastName" type="text" value={formData.lastName} onChange={handleChange} className="border-2 border-blue-300 rounded-md focus:border-blue-400 focus:outline-none transition duration-250" />
           <label htmlFor="address">Address:</label>
-          <input id="address" name="address" type="text" value={formData.address} onChange={handleChange} className="border" />
+          <input id="address" name="address" type="text" value={formData.address} onChange={handleChange} className="border-2 border-blue-300 rounded-md focus:border-blue-400 focus:outline-none transition duration-250" />
           <label htmlFor="phone">Phone:</label>
-          <input id="phone" name="phone" type="text" value={formData.phone} onChange={handleChange} className="border" />
-          <input type="submit" value="Search" />
+          <input id="phone" name="phone" type="text" value={formData.phone} onChange={handleChange} className="border-2 border-blue-300 rounded-md focus:border-blue-400 focus:outline-none transition duration-250" />
+          <input type="submit" value="Search" className="specialBtn text-blue-500 font-semibold hover:text-blue-700 py-2 my-5 rounded-lg" />
         </form>
       </main>
     </div>
