727243680d23677b9324482db08ef3e2ab7a13ad
Kyle <kb0531@cs.washington.edu>
2025-05-12 20:35:46 -0700

    trying to resolve nock issue

--- server/__test__/realFBM.int.test.ts
+++ server/__test__/realFBM.int.test.ts
@@ -2,35 +2,53 @@ import request from 'supertest';
 import express from 'express';
 import guestsRouter from '../routes/foodbankManager';
 
+// Disable nock if available (using ESM-safe import)
+const disableNock = async () => {
+  try {
+    const nock = await import('nock');
+    if (nock.isActive()) {
+      nock.restore();
+    }
+    nock.enableNetConnect();
+    console.log("Nock disabled for integration test");
+  } catch (err) {
+    console.warn("Could not disable nock:", err);
+  }
+};
+
 const app = express();
 app.use(express.json());
 app.use('/', guestsRouter);
 
 describe('Real FBM Integration Test', () => {
+  beforeAll(async () => {
+    await disableNock();
+  });
+
   it('adds a real guest to FoodBank Manager', async () => {
     const newGuest = {
-      firstname: "Sheldon",                     // string; required
-      lastname: "Plankton",                     // string; required
-      dob: "1990-10-10",                        // string; required; format YYYY-MM-DD
-      homeless: 0,                              // number; required; 0 = has residence
-      gender: "0",                              // string; required
-      race: "0",                                // string; required
-      household_total: 1,                       // number; required
-      language: "English",                      // string; required
-      cf_guests_45ae5f86e4: "In Country",       // required dropdown value
-      cf_guests_8e6f172090: "Small- 1 to 3",    // required dropdown value
-      cf_guests_e8827ca4cf: "0",                // string; optional
-      cf_guests_459373e1d1: [],                 // array; optional
-      street_address: "Chum Bucket HQ",         // string; required if homeless = 0
-      apartment: "",                            // optional
-      city: "Bikini Bottom",                    // optional
-      state: "Undersea",                        // optional
-      zipcode: "31415",                         // optional
-      county: "Pacific",                        // optional
-      phone: "",                                // optional
-      incomeTotal: "0.00",                      // stringified number
-      expenseTotal: "0.00",                     // stringified number
-      netTotal: "0.00"                          // stringified number
+      firstname: "Sheldon",
+      lastname: "Plankton",
+      dob: "1990-10-10",
+      homeless: 0,
+      gender: "0",
+      race: "0",
+      household_total: 1,
+      language: "English",
+      cf_guests_45ae5f86e4: "In Country",
+      cf_guests_8e6f172090: "Small- 1 to 3",
+      cf_guests_e8827ca4cf: "0",
+      cf_guests_459373e1d1: [],
+      street_address: "Chum Bucket HQ",
+      apartment: "",
+      city: "Bikini Bottom",
+      state: "Undersea",
+      zipcode: "31415",
+      county: "Pacific",
+      phone: "",
+      incomeTotal: "0.00",
+      expenseTotal: "0.00",
+      netTotal: "0.00"
     };
 
     console.log("SENDING TO FBM:", JSON.stringify(newGuest, null, 2));
