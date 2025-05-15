07fd52321c9bc224515d6a78626751cbbfcfe8b9
Jimmy Liu <jimmyliu20040@gmail.com>
2025-05-15 01:32:23 -0700

    Add helper function to parse the data from the response of search.

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -3,3 +3,38 @@
 export function demo_sum(a: number, b: number) {
     return a + b
 }
+
+interface table_entry {
+    id: number,
+    firstname: string,
+    lastname: number,
+    phone: string,
+    street_address: string,
+    othersHousehold: string
+}
+
+interface response_data {
+    results: table_entry[],
+    length: number
+}
+
+export function parseSearchResponseData(response: response_data) {
+    let len: number = response.results.length;
+
+    let newClients: any = [];
+    for (let i = 0; i < len; i++) {
+        newClients = [
+            ...newClients,
+            {
+                id: response.results[i].id,
+                firstname: response.results[i].firstname,
+                lastname: response.results[i].lastname,
+                street_address: response.results[i].street_address,
+                phone: response.results[i].phone,
+                othersHousehold: response.results[i].othersHousehold
+            }
+        ]
+    }
+
+    return newClients;
+}
