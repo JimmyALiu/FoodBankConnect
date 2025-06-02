94ba1324f67ab71e690ace3414bda4a0dc11135c
Jimmy Liu <jimmyliu20040@gmail.com>
2025-06-01 22:29:22 -0700

    Display error msg to search page.

--- client/src/Search.tsx
+++ client/src/Search.tsx
@@ -27,6 +27,8 @@ function Search({
     phone: ''
   });
 
+  const [errorMsg, setErrorMsg] = useState("");
+
   function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
     setFormData({
       ...formData,
@@ -37,22 +39,21 @@ function Search({
   function searchDatabase(): void {
     // validate form
     if (formData.firstName != "" && !/^[a-zA-Z]+$/.test(formData.firstName)) {
-      console.log("invalid first name");
+      setErrorMsg("Please enter a valid first name");
       return;
     }
 
     if (formData.lastName != "" && !/^[a-zA-Z]+$/.test(formData.lastName)) {
-      console.log("invalid first name");
+      setErrorMsg("Please enter a valid last name");
       return;
     }
 
     if (formData.phone != "" && !/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(formData.phone)) {
-      console.log("invalid phone");
+      setErrorMsg("Please enter a valid phone number");
       return;
     }
-
-    console.log('valid first')
-
+    
+    setErrorMsg("");
     setClients([]);
 
     // should send a request to the database
@@ -106,6 +107,10 @@ function Search({
           <input id="phone" name="phone" type="text" value={formData.phone} placeholder="123-456-7890" onChange={handleChange} className="px-2 py-1 border-2 border-blue-300 rounded-md focus:border-blue-400 focus:outline-none transition duration-250" />
           <input type="submit" value="Search" className="specialBtn text-blue-500 font-semibold hover:text-blue-700 py-2 my-5 rounded-lg" />
         </form>
+
+        <div className="mb-3 mt-5">
+          <p className="text-red-600">{errorMsg}</p>
+        </div>
 
         {/* search results */}
         <div className="overflow-hidden mt-10 shadow-md rounded-xl">
